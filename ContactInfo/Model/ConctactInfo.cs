d8d35d3ecce90cdb1873ee016b43949b8cf2a347
Tshepiso_Dev <TshepisoDev@outlook.com>
2023-07-25 11:28:19 +0200

    Add project files.

---
+++ ContactInfo/Model/ConctactInfo.cs
@@ -0,0 +1,19 @@
+﻿using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace ContactInfo.Model
+{
+	public class ConctactInfo
+	{
+		[BsonId]
+		[BsonRepresentation(BsonType.ObjectId)]
+		public string Id { get; set; }
+		[BsonElement("name")]
+		public string Name { get; set; } = string.Empty;
+		[BsonElement("email")]
+		public string Email { get; set; } = string.Empty;
+		[BsonElement("phoneNumber")]
+		public long Phone { get; set; }
+
+	}
+}
